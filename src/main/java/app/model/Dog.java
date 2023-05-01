0c0d7044b58436e45a067281e3e9c23abc8f2d4f
deepwaterinc <73400162+deepwaterinc@users.noreply.github.com>
2023-05-01 12:02:52 +0300

    Update Dog.java

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Component;
 public class Dog extends Animal {
     @Override
     public String toString() {
-        return "Im a Cat";
+        return "Im a Dog";
     }
 }
